271ea93d7c6c09ba3ec53582ba23590b45fa05a4
habuu <habuu@devserver.opintanner.fi>
2015-01-22 20:56:54 +0200

    Removed old templates, updated .gitignore

--- Handler/Test.hs
+++
@@ -1,9 +0,0 @@
-module Handler.Test where
-
-import Import
-
-getTestR :: Handler Html
-getTestR = error "Not yet implemented: getTestR"
-
-postTestR :: Handler Html
-postTestR = error "Not yet implemented: postTestR"
