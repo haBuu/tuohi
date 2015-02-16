5f4e3e58221ad63172dd995b3053fb2fbab2d442
habuu <habuu@devserver.opintanner.fi>
2015-02-16 18:39:15 +0200

    warp-TLS for production app

--- Application.hs
+++ Application.hs
@@ -21,6 +21,7 @@ import Network.Wai.Handler.Warp             (Settings, defaultSettings,
                                              defaultShouldDisplayException,
                                              runSettings, setHost,
                                              setOnException, setPort, getPort)
+import Network.Wai.Handler.WarpTLS
 import Network.Wai.Middleware.RequestLogger (Destination (Logger),
                                              IPAddrSource (..),
                                              OutputFormat (..), destination,
@@ -68,76 +69,80 @@ mkYesodDispatch "App" resourcesApp
 -- migrations handled by Yesod.
 makeFoundation :: AppSettings -> IO App
 makeFoundation appSettings = do
-    -- Some basic initializations: HTTP connection manager, logger, and static
-    -- subsite.
-    appHttpManager <- newManager
-    appLogger <- newStdoutLoggerSet defaultBufSize >>= makeYesodLogger
-    appStatic <-
-        (if appMutableStatic appSettings then staticDevel else static)
-        (appStaticDir appSettings)
+  -- Some basic initializations: HTTP connection manager, logger, and static
+  -- subsite.
+  appHttpManager <- newManager
+  appLogger <- newStdoutLoggerSet defaultBufSize >>= makeYesodLogger
+  appStatic <-
+    (if appMutableStatic appSettings then staticDevel else static)
+    (appStaticDir appSettings)
 
-    -- We need a log function to create a connection pool. We need a connection
-    -- pool to create our foundation. And we need our foundation to get a
-    -- logging function. To get out of this loop, we initially create a
-    -- temporary foundation without a real connection pool, get a log function
-    -- from there, and then create the real foundation.
-    let mkFoundation appConnPool = App {..}
-        tempFoundation = mkFoundation $ error "connPool forced in tempFoundation"
-        logFunc = messageLoggerSource tempFoundation appLogger
+  -- We need a log function to create a connection pool. We need a connection
+  -- pool to create our foundation. And we need our foundation to get a
+  -- logging function. To get out of this loop, we initially create a
+  -- temporary foundation without a real connection pool, get a log function
+  -- from there, and then create the real foundation.
+  let mkFoundation appConnPool = App {..}
+      tempFoundation = mkFoundation $ error "connPool forced in tempFoundation"
+      logFunc = messageLoggerSource tempFoundation appLogger
 
-    -- Create the database connection pool
-    pool <- flip runLoggingT logFunc $ createSqlitePool
-        (sqlDatabase $ appDatabaseConf appSettings)
-        (sqlPoolSize $ appDatabaseConf appSettings)
+  -- Create the database connection pool
+  pool <- flip runLoggingT logFunc $ createSqlitePool
+    (sqlDatabase $ appDatabaseConf appSettings)
+    (sqlPoolSize $ appDatabaseConf appSettings)
 
-    -- Perform database migration using our application's logging settings.
-    runLoggingT (runSqlPool (runMigration migrateAll) pool) logFunc
+  -- Perform database migration using our application's logging settings.
+  runLoggingT (runSqlPool (runMigration migrateAll) pool) logFunc
 
-    -- Return the foundation
-    return $ mkFoundation pool
+  -- Return the foundation
+  return $ mkFoundation pool
 
 -- | Convert our foundation to a WAI Application by calling @toWaiAppPlain@ and
 -- applyng some additional middlewares.
 makeApplication :: App -> IO Application
 makeApplication foundation = do
-    logWare <- mkRequestLogger def
-        { outputFormat =
-            if appDetailedRequestLogging $ appSettings foundation
-                then Detailed True
-                else Apache
-                        (if appIpFromHeader $ appSettings foundation
-                            then FromFallback
-                            else FromSocket)
-        , destination = Logger $ loggerSet $ appLogger foundation
-        }
+  logWare <- mkRequestLogger def
+    { outputFormat =
+      if appDetailedRequestLogging $ appSettings foundation
+        then Detailed True
+        else Apache
+              (if appIpFromHeader $ appSettings foundation
+                then FromFallback
+                else FromSocket)
+    , destination = Logger $ loggerSet $ appLogger foundation
+    }
 
-    -- Create the WAI application and apply middlewares
-    appPlain <- toWaiAppPlain foundation
-    return $ logWare $ defaultMiddlewaresNoLogging appPlain
+  -- Create the WAI application and apply middlewares
+  appPlain <- toWaiAppPlain foundation
+  return $ logWare $ defaultMiddlewaresNoLogging appPlain
 
 -- | Warp settings for the given foundation value.
 warpSettings :: App -> Settings
 warpSettings foundation =
-      setPort (appPort $ appSettings foundation)
-    $ setHost (appHost $ appSettings foundation)
-    $ setOnException (\_req e ->
-        when (defaultShouldDisplayException e) $ messageLoggerSource
-            foundation
-            (appLogger foundation)
-            $(qLocation >>= liftLoc)
-            "yesod"
-            LevelError
-            (toLogStr $ "Exception from Warp: " ++ show e))
-      defaultSettings
+    setPort (appPort $ appSettings foundation)
+  $ setHost (appHost $ appSettings foundation)
+  $ setOnException (\_req e ->
+      when (defaultShouldDisplayException e) $ messageLoggerSource
+        foundation
+        (appLogger foundation)
+        $(qLocation >>= liftLoc)
+        "yesod"
+        LevelError
+        (toLogStr $ "Exception from Warp: " ++ show e))
+    defaultSettings
+
+warpTLSSettings :: TLSSettings
+warpTLSSettings =
+  tlsSettings "config/certificate.pem" "config/key.pem"
 
 -- | For yesod devel, return the Warp settings and WAI Application.
 getApplicationDev :: IO (Settings, Application)
 getApplicationDev = do
-    settings <- getAppSettings
-    foundation <- makeFoundation settings
-    wsettings <- getDevSettings $ warpSettings foundation
-    app <- makeApplication foundation
-    return (wsettings, app)
+  settings <- getAppSettings
+  foundation <- makeFoundation settings
+  wsettings <- getDevSettings $ warpSettings foundation
+  app <- makeApplication foundation
+  return (wsettings, app)
 
 getAppSettings :: IO AppSettings
 getAppSettings = loadAppSettings [configSettingsYml] [] useEnv
@@ -149,22 +154,22 @@ develMain = develMainHelper getApplicationDev
 -- | The @main@ function for an executable running this site.
 appMain :: IO ()
 appMain = do
-    -- Get the settings from all relevant sources
-    settings <- loadAppSettingsArgs
-        -- fall back to compile-time values, set to [] to require values at runtime
-        [configSettingsYmlValue]
+  -- Get the settings from all relevant sources
+  settings <- loadAppSettingsArgs
+    -- fall back to compile-time values, set to [] to require values at runtime
+    [configSettingsYmlValue]
 
-        -- allow environment variables to override
-        useEnv
+    -- allow environment variables to override
+    useEnv
 
-    -- Generate the foundation from the settings
-    foundation <- makeFoundation settings
+  -- Generate the foundation from the settings
+  foundation <- makeFoundation settings
 
-    -- Generate a WAI Application from the foundation
-    app <- makeApplication foundation
+  -- Generate a WAI Application from the foundation
+  app <- makeApplication foundation
 
-    -- Run the application with Warp
-    runSettings (warpSettings foundation) app
+  -- Run the application with Warp-TLS
+  runTLS warpTLSSettings (warpSettings foundation) app
 
 
 --------------------------------------------------------------
@@ -172,11 +177,11 @@ appMain = do
 --------------------------------------------------------------
 getApplicationRepl :: IO (Int, App, Application)
 getApplicationRepl = do
-    settings <- getAppSettings
-    foundation <- makeFoundation settings
-    wsettings <- getDevSettings $ warpSettings foundation
-    app1 <- makeApplication foundation
-    return (getPort wsettings, foundation, app1)
+  settings <- getAppSettings
+  foundation <- makeFoundation settings
+  wsettings <- getDevSettings $ warpSettings foundation
+  app1 <- makeApplication foundation
+  return (getPort wsettings, foundation, app1)
 
 shutdownApp :: App -> IO ()
 shutdownApp _ = return ()
