5156330d21af9ab38a17bb574f2c31b3ae1fdde4
habuu <habuu@devserver.opintanner.fi>
2015-06-19 22:51:17 +0300

    Missing import

--- Application.hs
+++ Application.hs
@@ -58,6 +58,7 @@ import Handler.Handicaps
 import Handler.FinishedCompetitions
 import Handler.SignUp
 import Handler.EditCompetition
+import Handler.EventLog
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
