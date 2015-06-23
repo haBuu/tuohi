847dbaaa4624c8d5ea2953933e3305fb50250721
habuu <habuu@devserver.opintanner.fi>
2015-06-23 20:56:02 +0300

    Renamed input.julius to input-scores.julius

--- Handler/Scores.hs
+++ Handler/Scores.hs
@@ -3,6 +3,9 @@ module Handler.Scores where
 
 import Import
 
+import qualified Database.Esqueleto as E
+import Database.Esqueleto((^.))
+
 import Handler.CompetitionAuth
 import Handler.Forms
 import Model.RoundState(RoundState(..))
@@ -52,7 +55,14 @@ insertScore :: CompetitionId -> Score -> Handler ()
 insertScore cid score = runDB $
   -- score 0 is special value for deleting scores
   if scoreScore score == 0
-    then deleteBy $ UniqueScore (scoreRoundId score) (scoreHoleId score)
+    then do
+      mScore <- getBy $
+        UniqueScore (scoreRoundId score) (scoreHoleId score)
+      case mScore of
+        -- maybe not the best idea to delete update log with the score
+        -- but we will go with that for now
+        Just (Entity sid _) -> deleteCascade sid
+        Nothing -> return ()
     else do
       eScore <- insertBy score
       case eScore of
@@ -66,3 +76,59 @@ insertScore cid score = runDB $
             update key [ScoreScore =. scoreScore score]
             time <- liftIO getCurrentTime
             insert_ $ ScoreUpdateLog key cid time old new
+
+getScoreEditPlayersR :: CompetitionId -> Handler Html
+getScoreEditPlayersR cid = do
+  (competition, players) <- runDB $ do
+    c <- get404 cid
+    p <- confirmedPlayers cid
+    return (c, p)
+  defaultLayout $ do
+    setTitleI MsgEditScores
+    $(widgetFile "score-edit-players")
+
+getScoreEditPlayerR :: CompetitionId -> UserId -> Handler Html
+getScoreEditPlayerR cid uid = do
+  user <- runDB $ get404 uid
+  competition <- runDB $ get404 cid
+  let lid = competitionLayoutId competition
+  holes <- runDB $ selectList
+    [HoleLayoutId ==. lid] [Asc HoleNumber]
+  rounds <- runDB $ selectList
+    [ RoundCompetitionId ==. cid
+    , RoundUserId ==. uid
+    ]
+    [Asc RoundRoundnumber]
+  scores <- runDB $ selectList
+    [ScoreRoundId <-. (map (\(Entity rid _) -> rid) rounds)]
+    []
+  roundsAndForms <- forM rounds $ \(Entity rid round_) -> do
+    forms <- forM holes $ \(Entity hid hole) -> do
+      -- try to find existing score for the form
+      let mScore = flip find scores $ \(Entity _ score) ->
+            scoreRoundId score == rid && scoreHoleId score == hid
+      -- get actual value of the score from Maybe (Entity Score)
+      let value = fmap (scoreScore . entityVal) mScore
+      runFormPost $ scoreEditForm cid hid rid (holeNumber hole) value
+    return (roundRoundnumber round_, forms)
+  defaultLayout $ do
+    setTitleI MsgEditScores
+    $(widgetFile "input-scores")
+    $(widgetFile "score-edit")
+
+postScoreEditR :: CompetitionId -> RoundId -> HoleId -> Handler Html
+postScoreEditR cid rid hid = do
+  round_ <- runDB $ get404 rid
+  hole <- runDB $ get404 hid
+  ((result, _), _) <- runFormPost $
+    scoreEditForm cid hid rid (holeNumber hole) Nothing
+  formHandler result $ \res -> insertScore cid res
+  redirect $ ScoreEditPlayerR cid (roundUserId round_)
+
+confirmedPlayers :: CompetitionId -> DB [Entity User]
+confirmedPlayers cid = E.select $
+  E.from $ \(signUp, user) -> do
+    E.where_ $ signUp ^. SignUpUserId E.==. user ^. UserId
+    E.where_ $ signUp ^. SignUpCompetitionId E.==. E.val cid
+    E.where_ $ signUp ^. SignUpConfirmed E.==. E.val True
+    return user
